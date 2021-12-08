0ae25fbfa1112e50265b9de157444c55ff2945f8
Trevor Esparza <trevoresparza30@gmail.com>
2021-12-08 11:59:46 -0600

    add hello-asteroid java

--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -1,1 +1,18 @@
-console.log("Node is working");
+// console.log("Node is working");
+// "hello world!"
+// 'hello world'
+// 'hello isn\'t a world'
+// console.log('hello, this isn\'t a world');
+// console.log("I love hamburgers " + "with bacon, jalapenos, " + "mustard, mayo and lettuce. " + "I can do without the tomatoes and onions!");
+// console.log('this stuff is crazy cool');
+var favoritefood = "anything breakfast!";
+console.log("my favorite food is " + favoritefood + "!");
+favoritefood = "double quarter pounder with cheese from Mickey D's!";
+console.log("ok, change of plans. I'm jones'n for " + favoritefood);
+var phrase1 = "I'm really jones'n for a ";
+console.log(phrase1 + favoritefood);
+
+
+
+
+
