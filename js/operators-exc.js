3f93262196e0e9d8fe7ede52ecdb5de814925bc6
Trevor Esparza <trevoresparza30@gmail.com>
2021-12-09 11:52:09 -0600

    adding code to operator file

---
+++ js/operators-exc.js
@@ -0,0 +1,19 @@
+console.log('1' + 2);
+console.log(typeof '1' + 2);
+console.log(6 % 4);
+console.log('3 + 4 is') + 3 + 4;
+console.log(0 < 0);
+console.log("false" == false);
+console.log(true == 'true');
+console.log(5 >= -5);
+console.log(!false || false);
+console.log(true || "42");
+console.log(!true && !false);
+console.log(6 % 5);
+console.log(5 < 4 && 1 === 1);
+console.log(4 > 0 && 1 !== "1");
+console.log(6 % 3 === 0);
+console.log(5 % 2 !== 0);
+
+
+
