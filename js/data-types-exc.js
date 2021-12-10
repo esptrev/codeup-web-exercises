d5179530f9898bfeec0a4ecd3cdfe115747a76e6
Trevor Esparza <trevoresparza30@gmail.com>
2021-12-09 21:04:35 -0600

    finishes exc files

--- js/data-types-exc.js
+++ js/data-types-exc.js
@@ -1,2 +1,102 @@
-var sample = "Hello CodeUp";
-console.log(sample.length);
+// var sample = "Hello CodeUp";
+// console.log(sample.length);
+// console.log(sample.toUpperCase());
+// console.log(sample.toLowerCase());
+// var sample = sample + " Students!";
+// console.log(sample);
+// console.log(sample.replace('Students!' , 'Class!'));
+// console.log(sample.indexOf("C"));
+// console.log(sample.substr(sample.indexOf("C"), + 6)); //use substring alt sample.indexOf("P"), + 1
+
+
+//LM = 3 days
+// BB = 5 days
+//Herc = 1 day
+// PPD = 3
+// total = (lm + bb + hc)* ppd
+
+// let littleMermaid = 3;
+// let brotherBear = 5;
+// let hercules = 1
+// let pricePerDay = 3
+// var totalRentalFees = (littleMermaid + brotherBear + hercules) * pricePerDay;
+// console.log(totalRentalFees);
+
+//google is 400ph
+//amazon is 380ph
+//facebook is 350ph
+//google time is 6
+//amazaon time is 4
+//facebook time is 10
+// let faceBookTimeSheet = 10;
+// let amazonTimeSheet = 4;
+// let googleTimeSheet = 6
+// let fbHourly = 350
+// let azHourly = 380;
+// let ggHourly = 400
+// var takeHomePay = (faceBookTimeSheet * fbHourly) + (googleTimeSheet * ggHourly) + (amazonTimeSheet * azHourly);
+// console.log(takeHomePay);
+
+//class full = false
+//class time slot = false
+//class students < class full
+//max class students 100
+//schedule must be open
+// let maxClass = 100;
+// let classEnrollment = 10;
+// let scheduleOpen = true;
+// var allowedToRegister = (classEnrollment < maxClass) && scheduleOpen;
+// console.log(allowedToRegister);
+
+//must buy 2 or more
+//discount date has to be valid
+//premium nembers exempt
+// this is really an if else solution
+
+// let basicMembership = "b";
+// let premiumMembership = "p";
+// var qP = (x >= 2);
+// var x = 5;
+
+// let membershipLevel = true;
+// let quantityPurchased = false;
+// let offerValid = false;
+// var applyOffer = (membershipLevel || quantityPurchased) && offerValid;
+// console.log(applyOffer);
+
+
+// Create a variable that holds a boolean value for each of the following conditions:
+//
+//     the password must be at least 5 characters
+// the password must not include the username
+// the username must be no more than 20 characters
+// neither the username or password can start or end with whitespace
+
+var username = 'codeup';
+var password = 'notastrongpassword';
+var password = password.trim(); //trims whsp but doesn't prevent it,  sbould be if/else to end code
+var username = username.trim();
+// console.log(password);
+// console.log(username);
+var passLength = password.length;
+var noUserInPass = (password.indexOf(username) === -1); //keeps full username out of password, need multiple indexof to elimnate portions there of
+// console.log(noUserInPass);
+// console.log(passLength);
+var userLength = username.length;
+// console.log(userLength);
+var validPassword = (passLength >= 5 && userLength <= 20 && noUserInPass  );
+console.log(validPassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/variables-exc.js
+++ js/variables-exc.js
@@ -1,51 +1,71 @@
-let a = 1;
-let b = a++;
-let c = ++a;
+// let a = 1;
+// let b = a++;
+// let c = ++a;
 // what is the value of a, b, and c?
-//1,1,3
- console.log(a)
-
-
-let d = "hello";
-let e = false;
-d++;
-e++;
-console.log(d);
-
-var perplexed = 5; // perplexed is undefined (no value is assigned)
-perplexed + 2;
-console.log(perplexed);
-
-
-var price = 2.7;
-price.toFixed(2);
-console.log(price);
-
-console.log(isNaN(1));
-
-console.log(isNaN(""));
-
-console.log(isNaN("string"));
-
-console.log(isNaN("0"));
-
-console.log(isNaN("1"));
-
-console.log(isNaN("3.145"));
-
-
-
-console.log(isNaN(Number.MAX_VALUE));
-
-console.log(isNaN(Infinity));
-
-console.log(isNaN("true"));
-
-console.log(isNaN(true));
-
-console.log(isNaN("false"));
-
-console.log(isNaN(false));
-
+// 1,1,3
+ // console.log(a)
+//
+//
+// let d = "hello";
+// let e = false;
+// d++;
+// e++;
+// console.log(d);
+//
+// var perplexed = 5; // perplexed is undefined (no value is assigned)
+// perplexed + 2;
+// console.log(perplexed);
+//
+//
+// var price = 2.7;
+// price.toFixed(2);
+// console.log(price);
+//
+// console.log(isNaN(1));
+//
+// console.log(isNaN(""));
+//
+// console.log(isNaN("string"));
+//
+// console.log(isNaN("0"));
+//
+// console.log(isNaN("1"));
+//
+// console.log(isNaN("3.145"));
+//
+//
+//
+// console.log(isNaN(Number.MAX_VALUE));
+//
+// console.log(isNaN(Infinity));
+//
+// console.log(isNaN("true"));
+//
+// console.log(isNaN(true));
+//
+// console.log(isNaN("false"));
+//
+// console.log(isNaN(false));
+//
 // to illustrate why the isNaN() function is needed:
-NaN == NaN
+// NaN == NaN
+
+
+
+console.log(!true);
+
+console.log(!false);
+console.log(!!true);
+console.log(!!false);
+console.log(!!0);
+console.log(!!-0);
+console.log(!!1);
+console.log(!!-1);
+console.log(!!0.1);
+console.log(!!"hello");
+console.log(!!"");
+console.log(!!'');
+console.log(!!"false");
+console.log(!!"0");
+
+
