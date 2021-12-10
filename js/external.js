5a503de3f60af617ce7a1a57e4188777218bdf60
Trevor Esparza <trevoresparza30@gmail.com>
2021-12-10 08:49:19 -0600

    adding js files

---
+++ js/external.js
@@ -0,0 +1,12 @@
+"use strict";
+// console.log('Hello from JavaScript!');
+//
+// alert('Welcome to my Website!');
+//
+// var response = prompt("What is your favorite color?");
+//     if (response === 'blue'){
+//         console.log('That\'s awesome, ' + response, + 'is my favorite color too!');
+//     }else{
+//         console.log(response + " is a nice color.");
+//
+// }
