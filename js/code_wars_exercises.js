03478c7d3b8b4ad461f971748aee953164d684b4
Trevor Esparza <trevoresparza30@gmail.com>
2022-02-14 16:58:55 -0600

    working on weather map

--- js/code_wars_exercises.js
+++ js/code_wars_exercises.js
@@ -1,293 +1,294 @@
-//Convert Decimal to Binary then add, output should be string
-//Converts to Binary
-function decimalToBinary(decimal){
-    return (decimal >>> 0).toString(2);
-}
-console.log(decimalToBinary());
-//Adds binary
-function addBinary(a,b) {
-    return decimalToBinary(a+b);
-}
-console.log(addBinary(1,2));
-console.log(typeof addBinary());
-
-//Split the Digits then Square then and concatenate, return integer
-
-function squareDigits(num){
-    var array = num.toString().split('').map( function(int) {
-        var i = parseInt(int);
-        return i * i;
-    });
-
-    return parseInt(array.join(""));
-}
-
-//Accumulating characters Code should print out characters based on position in the string.
-// accum("abcd") -> "A-Bb-Ccc-Dddd"
-// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
-// accum("cwAt") -> "C-Ww-Aaa-Tttt"
-function accum(s) {
-    return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
-}
-console.log(accum('kdjdj'));
-
-function accum(str) {
-    var res = [];
-    for(var i = 0; i < str.length; i++) {
-        var row = '';
-        for(var j = 0; j < i + 1; j++) {
-            row += j===0 ? str[i].toUpperCase() : str[i].toLowerCase();
-        }
-        res.push(row);
-    }
-    return res.join('-');
-}
-console.log(accum('fgtyh'));
-
-//Code from Raymond on objects
-// for (const [key, value] of Object.entries(object1)) {
-//     console.log(`${key}: ${value}`);
+// //Convert Decimal to Binary then add, output should be string
+// //Converts to Binary
+// function decimalToBinary(decimal){
+//     return (decimal >>> 0).toString(2);
 // }
-// Write a function that returns the SUM of a sequence of numbers. This sequence is determined by three variables: start, finish, and step. The sequence begins at start, ends at finish, and goes up by step until it reaches finish or a number that when increased by step would go past finish.
-
-// Input: start: 1 finish: 4 step: 1 | Output: 10
-// Input: start: 4 finish: 10 step: | Output: 13
-
-function sum(sequence){
-    let total = 0;
-    for (let i = 0; i < sequence.length; i++) {
-        total += sequence[i];
-    }
-    console.log(total);
-}
-sum([1,2,3,4]);
-
-/* Given a string of any length with any characters in it, write a function to determine whether or not the string contains the whole word "english". The order of characters/spelling is important, a string "agkjnenglishsad" would return true while "asdneglihsth" would return false. Upper and lower case does not matter. Return values should be booleans (true/false).
-*/
-
-function findSubString(string){
-    let partial = 'english';
-   return !!string.toLowerCase().includes(partial);
-
-}
-console.log(findSubString("SMFENgliSHasnD"));
-// findSubString("");
-let scores =[10,5,20,20,4,5,2,25,1]
-function breakingRecords(scores) {
-    let maxCount = 0;
-    let minCount = 0;
-    let maxScore = scores[0]
-    let minScore = scores[0];
-    for (let i = 0; i < scores.length; i++) {
-        if (scores[i] < minScore) {
-            minScore = scores[i];
-            minCount++;
-        } else if(maxScore < scores[i]) {
-            maxScore = scores[i];
-            maxCount++;
-        }
-    }
-    return [maxCount,minCount];
-
-}
-console.log(breakingRecords(scores));
-
-
-// Remove all ! from string then add one to end
-function remove (string) {
-    let strArr = string.split('')
-    const vToRemove = '!';
-    const results = strArr.filter(function (x){
-        return  x !== vToRemove;
-    })
-    let finished = results.join('') + '!';
-    console.log(finished);
-}
-remove('!!!hi!!');
-
-// Take full name and return just initials
-
-function abbreviate (string){
-    let strRay = string.split('')
-    let initials = []
-    initials.push(strRay[0]);
-    let indSpace = strRay[strRay.indexOf(' ') + 1];
-    initials.push(indSpace);
-    let final = initials.join('');
-    return (final.toUpperCase());
-
-}
-abbreviate('trevo esparza')
-
-//Function to return Boolean if number is prime
-function isPrime(num){
-    if(num < 2){
-        return false;
-    }
-    if(num === 2){
-        return true;
-    }
-    if(num % 2 === 0){
-        return false;
-    }
-}
-
-
-
-
-
-
-// Function loneRanger, remove the element that occurs only once in the array
-let array = [12,1,12,3,12,1,1,2,3,3];
-function loneRanger(input){
-    for (let i = 0; i < input.length; i++) {
-        if(input.indexOf(input[i]) === input.lastIndexOf(input[i])){
-            return input[i];
-        }
-
-    }
-}
-console.log(loneRanger(array));
-
-
-
-// Find integers divisors
-function divisors(integer) {
-    let divArr = [];
-    for(let i = 2; i < integer; i++){
-        if(integer % i === 0){
-            divArr.push(i);
-        }
-
-    }
-    if(divArr.length === 0){
-        return integer + " is prime";
-    }
-    return divArr;
-}
-console.log(divisors(15));    /*   [3, 5]  */
-console.log(divisors(12));      /* [2, 3, 4, 6]*/
-console.log(divisors(13));     /*  ( "13 is prime")*/
-
-//Remove the last !
-function remove (string) {
-    if(string.charAt(string.length -1) === '!'){
-        return string.slice(0,-1);
-    }
-    return string;
-}
-console.log(remove("Hi!"));
-console.log(remove("Hi!!!"));
-console.log(remove("!Hi!"));
-console.log(remove("Hi"));
-console.log(remove("Hi! Hi!"));
-console.log(remove("!Hi"));
-
-// doTest("Hi!!!", "Hi!!");
-// doTest("!Hi", "!Hi");
-// doTest("!Hi!", "!Hi");
-// doTest("Hi! Hi!", "Hi! Hi");
-// doTest("Hi", "Hi");
-
-//Find sum of divisors 3/5
-function solution(number){
-    let sumOfDivisors = 0;
-    if(number < 0){
-        return 0;
-    }else{
-        for(i=3;i<number;i++){
-            if(i % 3 === 0 || i % 5 === 0){
-                sumOfDivisors += i;
-            }
-        }
-        return sumOfDivisors;
-    }
-}
-
-console.log(solution(15));
-
-//given two arrays, merge and find the median for the merged array
-
-let firstArray = [1,3,5,7,9,11,13];
-let secondArray = [2,4,6,8,10,12,14];
-
-function findMergedMedian(inputA, inputB){
-    for (let i = 0; i < inputA.length; i++) {
-        secondArray.push(inputA[i]);
-    }
-    let sortedArray = secondArray.sort((a,b) => a - b);
-    const middle = Math.floor(sortedArray.length / 2);
-    if(sortedArray.length % 2 === 0){
-        return((sortedArray[middle - 1] + sortedArray[middle]) / 2);
-    }
-    return sortedArray[middle];
-}
-console.log(findMergedMedian(firstArray,secondArray));
-
-// does first string contain all the letters of the second string
-// this one doesn't work
-function isItThere(inputA,inputB){
-    for (let i = 0; i < inputA.length; i++) {
-        if (inputB.indexOf(inputA[i]) <= -1) return false;
-    }
-    for (let i = 0; i < inputB.length; i++) {
-        if (inputA.indexOf(inputB[i]) <= -1) return false;
-    }
-    return true;
-}
-console.log(isItThere('trlevlor','rovert'))
-
-// iterate through the array adding every index to every index and then divide by k
-function divisibleSumPairs(n,k,array) {
-    var counter = 0;
-    for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-            console.log(array[i]);
-            let sum = array[i] + array[j];
-            console.log(array[j]);
-            if(sum % k === 0){
-                counter++
-            }
-        }
-    }
-    return (counter);
-}
-divisibleSumPairs(5,3,[1,2,3,4,5]);
-
-//////////// converting milliseconds to day/time  ////////////////
-
-// let milliseconds = 1644676477;
-
-// let convertedTime = Date(milliseconds);
+// console.log(decimalToBinary());
+// //Adds binary
+// function addBinary(a,b) {
+//     return decimalToBinary(a+b);
+// }
+// console.log(addBinary(1,2));
+// console.log(typeof addBinary());
+//
+// //Split the Digits then Square then and concatenate, return integer
+//
+// function squareDigits(num){
+//     var array = num.toString().split('').map( function(int) {
+//         var i = parseInt(int);
+//         return i * i;
+//     });
+//
+//     return parseInt(array.join(""));
+// }
+//
+// //Accumulating characters Code should print out characters based on position in the string.
+// // accum("abcd") -> "A-Bb-Ccc-Dddd"
+// // accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// // accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// function accum(s) {
+//     return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+// }
+// console.log(accum('kdjdj'));
+//
+// function accum(str) {
+//     var res = [];
+//     for(var i = 0; i < str.length; i++) {
+//         var row = '';
+//         for(var j = 0; j < i + 1; j++) {
+//             row += j===0 ? str[i].toUpperCase() : str[i].toLowerCase();
+//         }
+//         res.push(row);
+//     }
+//     return res.join('-');
+// }
+// console.log(accum('fgtyh'));
+//
+// //Code from Raymond on objects
+// // for (const [key, value] of Object.entries(object1)) {
+// //     console.log(`${key}: ${value}`);
+// // }
+// // Write a function that returns the SUM of a sequence of numbers. This sequence is determined by three variables: start, finish, and step. The sequence begins at start, ends at finish, and goes up by step until it reaches finish or a number that when increased by step would go past finish.
+//
+// // Input: start: 1 finish: 4 step: 1 | Output: 10
+// // Input: start: 4 finish: 10 step: | Output: 13
+//
+// function sum(sequence){
+//     let total = 0;
+//     for (let i = 0; i < sequence.length; i++) {
+//         total += sequence[i];
+//     }
+//     console.log(total);
+// }
+// sum([1,2,3,4]);
+//
+// /* Given a string of any length with any characters in it, write a function to determine whether or not the string contains the whole word "english". The order of characters/spelling is important, a string "agkjnenglishsad" would return true while "asdneglihsth" would return false. Upper and lower case does not matter. Return values should be booleans (true/false).
+// */
+//
+// function findSubString(string){
+//     let partial = 'english';
+//    return !!string.toLowerCase().includes(partial);
+//
+// }
+// console.log(findSubString("SMFENgliSHasnD"));
+// // findSubString("");
+// let scores =[10,5,20,20,4,5,2,25,1]
+// function breakingRecords(scores) {
+//     let maxCount = 0;
+//     let minCount = 0;
+//     let maxScore = scores[0]
+//     let minScore = scores[0];
+//     for (let i = 0; i < scores.length; i++) {
+//         if (scores[i] < minScore) {
+//             minScore = scores[i];
+//             minCount++;
+//         } else if(maxScore < scores[i]) {
+//             maxScore = scores[i];
+//             maxCount++;
+//         }
+//     }
+//     return [maxCount,minCount];
+//
+// }
+// console.log(breakingRecords(scores));
+//
+//
+// // Remove all ! from string then add one to end
+// function remove (string) {
+//     let strArr = string.split('')
+//     const vToRemove = '!';
+//     const results = strArr.filter(function (x){
+//         return  x !== vToRemove;
+//     })
+//     let finished = results.join('') + '!';
+//     console.log(finished);
+// }
+// remove('!!!hi!!');
+//
+// // Take full name and return just initials
+//
+// function abbreviate (string){
+//     let strRay = string.split('')
+//     let initials = []
+//     initials.push(strRay[0]);
+//     let indSpace = strRay[strRay.indexOf(' ') + 1];
+//     initials.push(indSpace);
+//     let final = initials.join('');
+//     return (final.toUpperCase());
+//
+// }
+// abbreviate('trevo esparza')
+//
+// //Function to return Boolean if number is prime
+// function isPrime(num){
+//     if(num < 2){
+//         return false;
+//     }
+//     if(num === 2){
+//         return true;
+//     }
+//     if(num % 2 === 0){
+//         return false;
+//     }
+// }
+//
+//
+//
+//
+//
+//
+// // Function loneRanger, remove the element that occurs only once in the array
+// let array = [12,1,12,3,12,1,1,2,3,3];
+// function loneRanger(input){
+//     for (let i = 0; i < input.length; i++) {
+//         if(input.indexOf(input[i]) === input.lastIndexOf(input[i])){
+//             return input[i];
+//         }
+//
+//     }
+// }
+// console.log(loneRanger(array));
+//
+//
+//
+// // Find integers divisors
+// function divisors(integer) {
+//     let divArr = [];
+//     for(let i = 2; i < integer; i++){
+//         if(integer % i === 0){
+//             divArr.push(i);
+//         }
+//
+//     }
+//     if(divArr.length === 0){
+//         return integer + " is prime";
+//     }
+//     return divArr;
+// }
+// console.log(divisors(15));    /*   [3, 5]  */
+// console.log(divisors(12));      /* [2, 3, 4, 6]*/
+// console.log(divisors(13));     /*  ( "13 is prime")*/
+//
+// //Remove the last !
+// function remove (string) {
+//     if(string.charAt(string.length -1) === '!'){
+//         return string.slice(0,-1);
+//     }
+//     return string;
+// }
+// console.log(remove("Hi!"));
+// console.log(remove("Hi!!!"));
+// console.log(remove("!Hi!"));
+// console.log(remove("Hi"));
+// console.log(remove("Hi! Hi!"));
+// console.log(remove("!Hi"));
+//
+// // doTest("Hi!!!", "Hi!!");
+// // doTest("!Hi", "!Hi");
+// // doTest("!Hi!", "!Hi");
+// // doTest("Hi! Hi!", "Hi! Hi");
+// // doTest("Hi", "Hi");
+//
+// //Find sum of divisors 3/5
+// function solution(number){
+//     let sumOfDivisors = 0;
+//     if(number < 0){
+//         return 0;
+//     }else{
+//         for(i=3;i<number;i++){
+//             if(i % 3 === 0 || i % 5 === 0){
+//                 sumOfDivisors += i;
+//             }
+//         }
+//         return sumOfDivisors;
+//     }
+// }
+//
+// console.log(solution(15));
+//
+// //given two arrays, merge and find the median for the merged array
+//
+// let firstArray = [1,3,5,7,9,11,13];
+// let secondArray = [2,4,6,8,10,12,14];
+//
+// function findMergedMedian(inputA, inputB){
+//     for (let i = 0; i < inputA.length; i++) {
+//         secondArray.push(inputA[i]);
+//     }
+//     let sortedArray = secondArray.sort((a,b) => a - b);
+//     const middle = Math.floor(sortedArray.length / 2);
+//     if(sortedArray.length % 2 === 0){
+//         return((sortedArray[middle - 1] + sortedArray[middle]) / 2);
+//     }
+//     return sortedArray[middle];
+// }
+// console.log(findMergedMedian(firstArray,secondArray));
+//
+// // does first string contain all the letters of the second string
+// // this one doesn't work
+// function isItThere(inputA,inputB){
+//     for (let i = 0; i < inputA.length; i++) {
+//         if (inputB.indexOf(inputA[i]) <= -1) return false;
+//     }
+//     for (let i = 0; i < inputB.length; i++) {
+//         if (inputA.indexOf(inputB[i]) <= -1) return false;
+//     }
+//     return true;
+// }
+// console.log(isItThere('trlevlor','rovert'))
+//
+// // iterate through the array adding every index to every index and then divide by k
+// function divisibleSumPairs(n,k,array) {
+//     var counter = 0;
+//     for (let i = 0; i < n; i++) {
+//         for (let j = i + 1; j < n; j++) {
+//             console.log(array[i]);
+//             let sum = array[i] + array[j];
+//             console.log(array[j]);
+//             if(sum % k === 0){
+//                 counter++
+//             }
+//         }
+//     }
+//     return (counter);
+// }
+// divisibleSumPairs(5,3,[1,2,3,4,5]);
+//
+//
+// /////////// Given a number array move all zeroes to end of array  ////////////////
+//
+// let originalArray = [0,1,0,2,0,3,0,4,0,5];
+//
+// originalArray.forEach(function (num){
+//     if(num === 0){
+//         originalArray.push(originalArray.splice(originalArray.indexOf(0), 1)[0]);
+//     }
+//     console.log(originalArray);
+// })
+//
+// //////////// converting milliseconds to day/time  ////////////////
+//
+// // let milliseconds = 1644676477;
+//
+// // let convertedTime = Date(milliseconds);
+// // console.log(convertedTime);
+// // let convertedTimeArray = (convertedTime.split(' '));
+// // console.log(convertedTimeArray);
+// // let dayOfWeek = convertedTimeArray[0];
+// // console.log(dayOfWeek);
+// // let militaryTime = convertedTimeArray[4];
+// // console.log(militaryTime);
+// let milliseconds = 164486160000;
+// let convertedTime = new Date();
 // console.log(convertedTime);
-// let convertedTimeArray = (convertedTime.split(' '));
-// console.log(convertedTimeArray);
-// let dayOfWeek = convertedTimeArray[0];
-// console.log(dayOfWeek);
-// let militaryTime = convertedTimeArray[4];
-// console.log(militaryTime);
-
-let convertedTime = new Date();
-console.log(convertedTime);
-let hours = convertedTime.getHours();
-console.log(hours)
-
-let milliseconds = 1644676477;
-
-let sampleTime = Date(milliseconds);
-console.log(sampleTime);
+// let hours = convertedTime.getHours();
+// console.log(hours)
 
 
-/////////// Given a number array move all zeroes to end of array  ////////////////
 
-let originalArray = [0,1,0,2,0,3,0,4,0,5];
-
-originalArray.forEach(function (num){
-    if(num === 0){
-        originalArray.push(originalArray.splice(originalArray.indexOf(0), 1)[0]);
-    }
-    console.log(originalArray);
-})
+// let sampleTime = Date(milliseconds);
+// console.log(sampleTime);
+//
 
 
 
--- js/weather_map_js.js
+++ js/weather_map_js.js
@@ -1,9 +1,18 @@
-(function(){
+(function () {
     'use strict';
     const OPEN_WEATHER_APPID = TREVORS_WEATHER_MAP_KEY;
     mapboxgl.accessToken = TREVORS_MAP_TOKEN;
-    const OPTIMAL_ZOOM_LEVEL = 12;
-    const STARTING_COORDS = [-98.45, 29.74];
+    const OPTIMAL_ZOOM_LEVEL = 20;
+    const STARTING_COORDS = [-98.4936, 29.4241];
+    const weatherURL = `http://api.openweathermap.org/data/2.5/onecall`;
+    let dailyWeatherInfo = [];
+
+    var weatherOptions = {
+        lat: 29.4241, /* i want coords to feed from map center */
+        lon: -98.4936,
+        appid: TREVORS_WEATHER_MAP_KEY,
+        units: "imperial"
+    };
 
     const map = new mapboxgl.Map({
         container: 'mapDiv', // container ID
@@ -12,17 +21,19 @@
         zoom: 11 // starting zoom
     });
 
-    let marker = '';  /* declaring marker variable so i can use it */
-
+////// CENTERS MAP AND SETS MARKER AT CENTER WHEN MAP STOPS MOVING, ALLOWS MARKER TO BE SET USING MAPBOX GEOCODE CONTROL ///////
     function onMoveEnd() {
         var coords = map.getCenter();
         marker.setLngLat(coords);
         makePopUpForCoords(coords)
-        console.log(coords);
+        // console.log(coords);
+        // appendWeather();
+        dailyWeatherUpdates();
     }
+
     map.on('moveend', onMoveEnd);
 
-    /* adding geocontrol box */
+    ////////MAPBOX GEOCODE CONTROLLER, COPIED DIRECTLY FROM WEBSITE//////////
 
     map.addControl(
         new MapboxGeocoder({
@@ -32,23 +43,26 @@
             mapboxgl: mapboxgl,
             marker: false,
         })
-
     );
-    marker = new mapboxgl.Marker({
+    var marker = new mapboxgl.Marker({
         draggable: true
-    }).setLngLat(STARTING_COORDS)
+    })
+        .setLngLat(STARTING_COORDS)
         .addTo(map);
-
+    marker.on(`load`, loadWeather());
     /*THE 'DRAGEND' IS FROM MAPBOX */
     marker.on('dragend', endOfMarkerDrag);
 
+    //// SETS MARKER AT END OF MARKER DRAG /////
     function endOfMarkerDrag() {
         const coords = marker.getLngLat();
-        console.log(results);
+        weatherOptions.lat = coords.lat;
+        weatherOptions.lon = coords.lng;
 
         /* Calling popup function with end of marker drag */
         makePopUpForCoords(coords);
     }
+
     /* POPUP AND ATTACHMENT TO COORDS */
 
     function makePopUpForCoords(coords) {
@@ -57,24 +71,121 @@
         const popup = new mapboxgl.Popup({closeOnClick: false})
             .setLngLat(coordArray)
             .setHTML(`${coordArray}`)
-        // .addTo(map); /* dont need to add to map, adding to marker*/
+        weatherOptions.lat = coordArray[1];
+        weatherOptions.lon = coordArray[0];
         marker.setPopup(popup);
         popup.addTo(map);
-        // doForecastingStuffForCoords(coordArray);
+        loadWeather();
+    }
+
+
+    function loadWeather() {
+        $.get(weatherURL, weatherOptions).done(function (data) {
+            console.log(data);
+            dailyWeatherInfo = data.daily;
+            console.log(dailyWeatherInfo);
+            convertWeatherData(dailyWeatherInfo);
+            dailyWeatherUpdates(dailyWeatherInfo);
+
+        })
+
+    }
+
+    function dailyWeatherUpdates(weather) {
+        $(`#cardStack`).html('');
+        weather.forEach(function (day) {
+            console.log(day);
+            var dailyHTML = createDailyForecastHTML(day);
+            $(`#cardStack`).append(dailyHTML);
+        })
+
+    }
+
+
+    function createDailyForecastHTML(singleDay) {
+
+        // language = HTML
+        var html = `
+			<div class="col-4">
+				<div id="dayOfWeek" class="card-body">${singleDay.dt}</div>
+				<div id="cardBody" class="card-body">
+					<div id="highLow">H:${singleDay.temp.max} / L:${singleDay.temp.min}</div>
+					<div id="icon">${singleDay.weather[0].icon}</div>
+					<div id="forecast">${singleDay.weather[0].description}</div>
+					<div id="wind">${singleDay.wind_speed} / ${singleDay.wind_deg}</div>
+					<div id="pressure">${singleDay.pressure}</div>
+					<div id="sun">${singleDay.sunrise}/${singleDay.sunset}</div>
+				</div>
+			</div>`
+
+
+        return html;
+
+    }
+
+
+
+//// delete my appendweather function///////
+
+    function convertWeatherData(days) {
+        let dayOfWeek = (days.temp);
+        console.log(dayOfWeek)
+
     }
 
 
 
 
 
-    $.get("http://api.openweathermap.org/data/2.5/onecall", {
-        APPID: OPEN_WEATHER_APPID,
-        lat:    32.7157,  /* i want coords to feed from map center */
-        lon:   -117.1611,
-        units: "imperial"
-    }).done(onSuccessfulLoad)
 
-    
+        // let utcConversion = days[0].dt
+        // const myDate = new Date(1644516000 * 1000);
+        // console.log(myDate);
+//         const dayOfWeek = getDayOfWeek(myDate.getDay());
+//         console.log(myDate.toString().substr(0, 3));
+//         console.log(setTime('America/Chicago'))
+
+        // const myDate = new Date(data.daily[0].dt);
+        // console.log(days[0].temp.max);
+
+    //     let dayOfWeek = ((data.daily.dt) * 1000);  /* need to convert */
+    //  //   $('#dayOfWeek').html(`${dayOfWeek}`);
+    //
+    //     let highTemp = data.daily[1].temp.max;  /* need to round */
+    //     let lowTemp = data.daily[1].temp.min;
+    //   //  $('#highLow').html(`H: ${highTemp}, L:${lowTemp}`);
+    //
+    //     let icon = data.daily[1].weather[0].icon;  /* need to convert # to jpeg */
+    //  //   $('#icon').html(`${icon}`);
+    //
+    //     let forecast = data.daily[1].weather[0].description;
+    //  //   $('#forecast').html(`${forecast}`);
+    //
+    //     let windSpeed = data.daily[1].wind_speed;
+    //     let windDirection = data.daily[1].wind_deg;
+    //  //   $('#wind').html(`${windSpeed}:${windDirection}`);
+    //
+    //     let atmosPressure = data.daily[1].pressure;
+    // //    $('#pressure').html(`${atmosPressure}`);
+    //
+    //     let dawnDay = data.daily[1].sunrise;
+    //
+    //     console.log(`i'm looking for this ${Date(dawnDay)}`);
+    //     console.log(Date(data.current.dt));
+    //     let duskDay = data.daily[1].sunset;
+    //    $('#sun').html(`${dawnDay}, ${duskDay}`);
+
+        // ////////USE THIS FUNCTION TO CONVERT DEGREES TO HEADING///////////
+        // function degToCompass(num) {
+        //     var val = Math.floor((num / 22.5) + 0.5);
+        //     var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+        //     return arr[(val % 16)];
+        // }
+
+        ////////////USE TO CONVERT MILLIBARS TO INCHES MERCURY//////////////
+    ////////    To convert millibars to inches of mercury, millibar * 0.0295301./////////
+
+
 
 
     function onSuccessfulLoad(data) {
@@ -82,45 +193,46 @@
         console.log('Diving in - here is current information: ', data.current);
         console.log('A step further - information for tomorrow: ', data.daily[1]);
 
-        let dayOfWeek = data.current.dt;  /* need to convert */
-        $('#dayOfWeek').html(`${dayOfWeek}`);
 
-        let highTemp = data.daily[1].temp.max;  /* need to round */
-        let lowTemp = data.daily[1].temp.min;
-        $('#highLow').html(`H: ${highTemp}, L:${lowTemp}`);
-
-        let icon = data.daily[1].weather[0].icon;  /* need to convert # to jpeg */
-        $('#icon').html(`${icon}`);
-
-        let forecast = data.daily[1].weather[0].description;
-        $('#forecast').html(`${forecast}`);
-
-        let windSpeed = data.daily[1].wind_speed;
-        let windDirection = data.daily[1].wind_deg;
-        $('#wind').html(`${windSpeed}:${windDirection}`);
-
-        let atmosPressure = data.daily[1].pressure;
-        $('#pressure').html(`${atmosPressure}`);
-
-        let dawnDay = data.daily[1].sunrise;
-
-        console.log(`i'm looking for this ${Date(dawnDay)}`);
-        console.log(Date(data.current.dt));
-        let duskDay = data.daily[1].sunset;
-        $('#sun').html(`${dawnDay}, ${duskDay}`);
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 })();
+
+// (function() {
+//     const OPENWEATHER_KEY = 'c44633dab287ad6aa0232cce2b4a4815';
+//
+//     function getDayOfWeek(dayNumber) {
+//         if(dayNumber === 4) {
+//             return "Thu";
+//         } else {
+//             return "Unknown";
+//         }
+//     }
+//
+//     function setTime(timeZ) {
+//         return new Intl.DateTimeFormat(navigator.language, {
+//             timeZone: timeZ,
+//             year: 'numeric',
+//             month: 'numeric',
+//             day: 'numeric',
+//             hour: 'numeric',
+//             minute: 'numeric'
+//         }).format(new Date());
+//     }
+//
+//     $(document).ready(function() {
+//
+//         $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=29.4241&lon=-98.4936&exclude=hourly,minutely&appid=${OPENWEATHER_KEY}&units=imperial`)
+//             .done(function (data) {
+//                 console.log(data);
+//             });
+//
+//         // const dateMS = ;
+//         const myDate = new Date(1644516000 * 1000);
+//         const dayOfWeek = getDayOfWeek(myDate.getDay());
+//         console.log(myDate.toString().substr(0, 3));
+//         console.log(setTime('America/Chicago'))
+//     });
+//
+// })();
