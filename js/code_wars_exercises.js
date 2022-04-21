b3b1d37fd50e951cf571884845ecc15b269d6cbe
Trevor Esparza <trevoresparza30@gmail.com>
2022-04-21 09:06:34 -0500

    little js practice

--- js/code_wars_exercises.js
+++ js/code_wars_exercises.js
@@ -408,3 +408,15 @@ const mergeSort = arr => {
 const unsortedArr = [31, 27, 28, 42, 13, 8, 11, 30,51,52,53,54, 17, 41, 15, 43, 1, 36, 9, 16, 20, 35, 48, 37, 7, 26, 34, 21, 22, 6, 29, 32, 49, 10, 12, 19, 24, 38, 5, 14, 44, 40, 3, 50, 46, 25, 18, 33, 47, 4, 45, 39, 23, 2];
 console.log(mergeSort(unsortedArr));
 
+const greeting = "      Happy     Tuesday    !    ";
+console.log(greeting.trim());
+
+
+console.log([1,2,3,4,5].fill(0,1,3));
+
+console.log(parseFloat(7.5/10));
+
+
+console.log("trevor")
+console.log([] == ![]);
+
